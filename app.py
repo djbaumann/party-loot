85cf527f5a9ebb695ae072beb7b96758a0e3390d
David Baumann <david_baumann@student.uml.edu>
2018-01-19 15:54:24 -0500

    init

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "welcome!"
